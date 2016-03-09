75fa7fc5543919d491963d810597c2800b0c0ed6
Lennart Nachtigall <firesurfer65@yahoo.de>
2016-03-09 19:19:04 +0100

    fixed compiler error

--- src/ros2_simple_logger/ConsoleColor.cpp
+++ src/ros2_simple_logger/ConsoleColor.cpp
@@ -1,7 +1,7 @@
 #include "ros2_simple_logger/ConsoleColor.h"
 
 
-std::__cxx11::string printInColor(const std::__cxx11::string &str, ConsoleColor color)
+std::string printInColor(const std::string &str, ConsoleColor color)
 {
     std::string col = "\033[" + std::to_string(color) + "m";
     return col + str + "\033[" + std::to_string(BG_DEFAULT) + "m";
